ae690086945a8978e1df0e11787df604940adc9d
Amin Basati <mohamadamin386mm@gmail.com>
2023-07-07 18:24:02 +0330

    add style to the coin list

--- src/Components/API.js
+++ src/Components/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=22&page=1&sparkline=false&locale=en';
+const BASE_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=16&page=1&sparkline=false&locale=en';
 
 const API = async () => {
     const getData = await axios.get(`${BASE_URL}`);
--- src/Components/coinList.js
+++ src/Components/coinList.js
@@ -18,46 +18,65 @@ const CoinList = () => {
     };
 
     const searchCoins = coin.filter(item => item.name.toUpperCase().includes(search.toUpperCase()));
-    const minPage = searchCoins.slice(0 , 9);
+    const minPage = searchCoins.slice(0 , 6);
 
     return (
-        <div>
-            <input type="text" value={search} onChange={searchHandler} placeholder="جستجو ..."/>
+        <div className="mt-20 sm:mt-36 font-vazirMedium text-slate-900 text-sm">
 
-            <div>
+            {/* Search */}
+            <div className="max-w-[80%] 500:max-w-[60%] 1120:max-w-[50%] mx-auto">
+                <input type="text" value={search} onChange={searchHandler} placeholder="جستجو ..." className="w-full
+                 outline-none bg-slate-200 h-12 500:h-14 1120:h-16 pr-2 rounded-lg placeholder:text-slate-900 1120:text-[16px]"/>
+            </div>
 
-                <div>
+            {/* ListCoin */}
+            <div className="border border-solid border-gray-200 rounded-lg p-2.5 max-w-[90%] sm:max-w-[70%]
+             shadow-xl mx-auto mt-10">
+
+                <div className="hidden 1120:flex justify-between text-[16px] about-list-coin bg-slate-100 rounded-md
+                 m-3 p-5">
                     <span>نماد</span>
                     <span>نام ارز</span>
                     <span>آخرین‌قیمت</span>
                     <span>تغییرات روزانه</span>
                 </div>
 
-                ‌{coin && showAll ? searchCoins.map(item => 
-                    <div key={Math.random() * 100}>
-                        <img src={item.image} alt="logo-coin"/>
-                        <div>
-                            <span>{item.name}</span>
-                            <span>{item.symbol}</span>
-                        </div>
-                        <span>{item.current_price}</span>
-                        <span>{item.market_cap_change_percentage_24h.toFixed(2)}</span>
-                    </div>     
-                ) :  minPage.map(item => 
-                    <div key={Math.random() * 100}>
-                        <img src={item.image} alt="logo-coin"/>
-                        <div>
-                            <span>{item.name}</span>
-                            <span>{item.symbol}</span>
-                        </div>
-                        <span>{item.current_price}</span>
-                        <span>{item.market_cap_change_percentage_24h.toFixed(2)}</span>
-                    </div>     
+                ‌{coin && showAll ? 
+                    searchCoins.map(item => 
+                        <div key={Math.random() * 100} className="list-coin">
+                            <img src={item.image} alt="logo-coin" className="w-12 sm:w-14"/>
+                            <div className="flex items-center">
+                                <span className="hidden sm:flex sm:text-sm text-gray-500 ml-4">{item.symbol}</span>
+                                <span className="sm:text-[15px] 1120:text-[17px]">{item.name}</span>
+                            </div>
+                            <span className="text-gray-400 sm:text-[15px]">$  {item.current_price}</span>
+                            <span className={`${item.market_cap_change_percentage_24h >= 0 ? "Positive" : "negative"}`} dir="ltr">
+                                {item.market_cap_change_percentage_24h.toFixed(2)}
+                            </span>
+                        </div>     
+                ) 
+                :  
+                    minPage.map(item => 
+                        <div key={Math.random() * 100} className="list-coin">
+                            <img src={item.image} alt="logo-coin" className="w-12 sm:w-14" />
+                            <div className="flex items-center">
+                                <span className="hidden sm:flex sm:text-sm text-gray-500 ml-4">{item.symbol}</span>
+                                <span className="sm:text-[15px] 1120:text-[17px]">{item.name}</span>
+                            </div>
+                            <span className="text-gray-400 sm:text-[15px]">$  {item.current_price}</span>
+                            <span className={`${item.market_cap_change_percentage_24h >= 0 ? "Positive" : "negative"}`} dir="ltr">
+                                {item.market_cap_change_percentage_24h.toFixed(2)}
+                            </span>
+                        </div>     
                 )}
 
             </div>
 
-            <div onClick={() => setShowAll(true)}>مشاهده ارزهای بیشتر</div>
+            {/* ShowAllBotton */}
+            <div onClick={() => setShowAll(true)} className="max-w-[80%] 500:max-w-[50%] 1120:max-w-[20%] bg-blue-500 cursor-pointer hover:bg-blue-600
+             font-vazirMedium text-white 1120:text-[16px] h-14 rounded-lg flex justify-center items-center mx-auto mt-10 mb-5 transition-all">
+                <p>مشاهده ارزهای بیشتر</p>
+            </div>
         </div>
     );
 };
