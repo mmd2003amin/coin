35e6f9e6f6b53fd4b26dd486a89993d666040913
Amin Basati <mohamadamin386mm@gmail.com>
2023-07-08 14:42:52 +0330

    add style to the Cards

--- src/Components/card.js
+++ src/Components/card.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Card = ({image , about , header}) => {
     return (
-        <div>
-            <img src={image} alt="image" />
-            <h1>{header}</h1>
-            <p>{about}</p>
+        <div className="max-w-fit mx-auto 1120:m-4 rounded-3xl h-fit p-5 bg-gray-100 mb-7 border border-solid
+        border-gray-300 hover:scale-110 duration-500 ">
+            <img src={image} alt="image" className="mx-auto w-44 mt-6" />
+            <h1 className="text-center font-black text-lg mt-8">{header}</h1>
+            <p className="mx-auto mt-5 text-gray-400 font-vazirRegular text-sm text-center mb-6 max-w-[97%]">{about}</p>
         </div>
     );
 };
--- src/Components/cards.js
+++ src/Components/cards.js
@@ -7,10 +7,10 @@ const Cards = () => {
     const [ data , setData ] = useState(dataCarts);
 
     return (
-        <div>
-            <h1>پلتفرم حرفه ای خرید و فروش ارز دیجیتال</h1>
-            <h5>چرا باید صرافی همتاپی را انتخاب کنیم ؟</h5>
-            <div>
+        <div className="mt-32 mb-10 max-w-[90%] sm:max-w-[70%] 1120:max-w-[85%] mx-auto font-vazirMedium">
+            <h1 className="text-center mb-7 text-[22px] 1120:text-[26px] font-black">پلتفرم حرفه ای خرید و فروش ارز دیجیتال</h1>
+            <h5 className="text-center mb-14 text-sm 1120:text-lg text-gray-500 font-vazirRegular">چرا باید صرافی همتاپی را انتخاب کنیم ؟</h5>
+            <div className="1120:flex">
                 {data.map(item => <Card key={Math.random() * 100} image={item.image} about={item.about} header={item.header}/>)}
             </div>
         </div>
